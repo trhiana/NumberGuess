4244ad42968933ded4da6e18d64d633741d1a68b
trhiana <trhiana@rocketmail.com>
2021-02-07 02:23:46 +0300

    Added variables

--- NumberGuess.java
+++ NumberGuess.java
@@ -1,7 +1,17 @@
+/**
+ * @author Rhiana Thelemaque
+ */
 
+
+/**
+ * Import packages
+ */
 
 public class NumberGuess{
     public static void main(String[] args) {
-        
+        int number = (int) (Math.random() * 100 + 1);
+        int answer = 0;
+        System.out.println("The correct guess would be " + number);
+        int count = 1;
     }
 }
