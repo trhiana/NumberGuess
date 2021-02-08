5042828ab1592989168846cff80207c16ca69168
trhiana <trhiana@rocketmail.com>
2021-02-08 22:13:25 +0300

    Created determineGuess method

--- NumberGuess.java
+++ NumberGuess.java
@@ -6,6 +6,8 @@
 /**
  * Import packages
  */
+import javax.swing.JOptionPane;
+
 
 public class NumberGuess{
     public static void main(String[] args) {
@@ -13,5 +15,18 @@ public class NumberGuess{
         int answer = 0;
         System.out.println("The correct guess would be " + number);
         int count = 1;
+
+        while(answer != number) {
+            String response = JOptionPane.showInputDialog(null, "Enter a guess between 1 and 100: ", "Guessing Game", 3);
+            answer = Integer.parseInt(response);
+            JOptionPane.showMessageDialog(null, "" + determineGuess(answer, number, count));
+            count ++;
+        }
+    }
+
+    public static String determineGuess(int answer, int number, int count){
+        if (answer <= 0 || answer > 100){
+            return "Invalid guess";
+        }
     }
 }
